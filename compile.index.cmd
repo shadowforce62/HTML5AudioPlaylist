2cdad1810508f190ef88cd3311d3d77e5bd1347b
shadowforce62 <shadowforce62@users.noreply.github.com>
2019-07-14 05:30:07 -0500

    Fixed Bad Spacing
    For Some Reason Kate Decided that when i copied and pasted it in RAW form it would add a bunch of new lines

--- compile.index.cmd
+++ compile.index.cmd
@@ -1,90 +1,37 @@
-Echo ^<!DOCTYPE html^>
->
-index.html
-Echo ^<html lang="en"^>
->
->index.html
-Echo ^<head^>
->
->index.html
-Echo    ^<meta charset="UTF-8"^>
->
->index.html
-Echo    ^<title^>HTML5 Audio Player^</title^>
->
->index.html
-Echo    ^<style^>
->
->index.html
-Echo        #playlist{
->
->index.html
-Echo            list-style: none;
->
->index.html
-Echo        }
->
->index.html
-Echo        #playlist li a{
->
->index.html
-Echo            color:black;
->
->index.html
-Echo            text-decoration: none;
->
->index.html
-Echo        }
->
->index.html
-Echo        #playlist .current-song a{
->
->index.html
-Echo            color:blue;
->
->index.html
-Echo        }
->
->index.html
-Echo    ^</style^>>
->index.html
-Echo    ^<!-- Source Code From YouTube.com/MicroTechTutorials  you may remove this message on your webpage but please do not redistribtue --^>>
->index.html
-Echo ^</head^>
->
->index.html
-Echo ^<body^>
->
->index.html
-Echo    ^<p^>Demo Music From ^<a href="http://incompetech.com"^>incompetech.com^</a^>^</p^>>
->index.html
-Echo    ^<audio src="" controls id="audioPlayer"^>>
->index.html
-Echo        Sorry, your browser doesn't support html5!>
->index.html
-Echo    ^</audio^>>
->index.html
-Echo    ^<ul id="playlist"^>>
->index.html
-Echo       ^<li class="current-song"^>^<a href="Exit%20the%20Premises.mp3"^>Exit the Premises^</a^>^</li^>>
->index.html
-Echo        ^<li^>^<a href="Severe%20Tire%20Damage.mp3"^>Severe Tire Damage^</a^>^</li^>>
->index.html
-Echo        ^<li^>^<a href="Broken%20Reality.mp3"^>Broken Reality^</a^>^</li^>>
->index.html
-Echo    ^</ul^>>
->index.html
-Echo    ^<script src="https://code.jquery.com/jquery-2.2.0.js"^>^</script^>>
->index.html
-Echo    ^<script src="audioPlayer.js"^>^</script^>>
->index.html
-Echo    ^<script^>>
->index.html
-Echo        // loads the audio player>
->index.html
+Echo ^<!DOCTYPE html^>>index.html
+Echo ^<html lang=^"en^"^>>>index.html
+Echo ^<head^>>>index.html
+Echo    ^<meta charset=^"UTF-8^"^>>>index.html
+Echo    ^<title^>HTML5 Audio Player^</title^>>>index.html
+Echo    ^<style^>>>index.html
+Echo        #playlist{>>index.html
+Echo            list-style: none;>>index.html
+Echo        }>>index.html
+Echo        #playlist li a{>>index.html
+Echo            color:black;>>index.html
+Echo            text-decoration: none;>>index.html
+Echo        }>>index.html
+Echo        #playlist .current-song a{>>index.html
+Echo            color:blue;>>index.html
+Echo        }>>index.html
+Echo    ^</style^>>>index.html
+Echo    ^<!-- Source Code From YouTube.com/MicroTechTutorials  you may remove this message on your webpage but please do not redistribtue --^>>>index.html
+Echo ^</head^>>>index.html
+Echo ^<body^>>>index.html
+Echo    ^<p^>Demo Music From ^<a href=^"http://incompetech.com^"^>incompetech.com^</a^>^</p^>>>index.html
+Echo    ^<audio src=^"^" controls id=^"audioPlayer^"^>>>index.html
+Echo        Sorry, your browser doesn't support html5!>>index.html
+Echo    ^</audio^>>>index.html
+Echo    ^<ul id=^"playlist^"^>>>index.html
+Echo       ^<li class=^"current-song^"^>^<a href=^"Exit%20the%20Premises.mp3^"^>Exit the Premises^</a^>^</li^>>>index.html
+Echo        ^<li^>^<a href=^"Severe%20Tire%20Damage.mp3^"^>Severe Tire Damage^</a^>^</li^>>>index.html
+Echo        ^<li^>^<a href=^"Broken%20Reality.mp3^"^>Broken Reality^</a^>^</li^>>>index.html
+Echo    ^</ul^>>>index.html
+Echo    ^<script src=^"https://code.jquery.com/jquery-2.2.0.js^"^>^</script^>>>index.html
+Echo    ^<script src=^"audioPlayer.js^"^>^</script^>>>index.html
+Echo    ^<script^>>>index.html
+Echo        // loads the audio player>>index.html
 Echo        audioPlayer();>>index.html
-Echo    ^</script^>>
->index.html
+Echo    ^</script^>>>index.html
 Echo ^</body^>>>index.html
-Echo ^</html^>>
->index.html
+Echo ^</html^>>>index.html
